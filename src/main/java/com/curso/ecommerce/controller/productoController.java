4911eabfd957a23419832f449d33728a8b6553fe
santiago orjuela <santiagoorjuela812@gmail.com>
2024-04-26 19:16:58 -0500

    creacion de guardado producto

--- src/main/java/com/curso/ecommerce/controller/productoController.java
+++ src/main/java/com/curso/ecommerce/controller/productoController.java
@@ -1,18 +1,42 @@
 package com.curso.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.service.ProductoService;
 
 
 @Controller
 @RequestMapping("/productos")
 public class productoController {
+	private final Logger LOGGER =LoggerFactory.getLogger(productoController.class);
 	
-
+	@Autowired
+	private ProductoService productoService;
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
+	}
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		LOGGER.info("ESTE ES EL OBJETO PRODUCTO {}",producto.toString());
+		Usuario u =new Usuario();
+		u.setId(1);
+		producto.setUsuario(u);
+		productoService.save(producto);
+
+		return "redirect:/productos";
 	}
 }
--- src/main/java/com/curso/ecommerce/service/ProductoServiceImpl.java
+++ src/main/java/com/curso/ecommerce/service/ProductoServiceImpl.java
@@ -3,10 +3,12 @@ package com.curso.ecommerce.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.repository.ProductoRepository;
 
+@Service
 public class ProductoServiceImpl implements ProductoService  {
 	
 	@Autowired
